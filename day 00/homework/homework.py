be868fbba0a257b902bc181ba896bac33131335f
kiravalidze <yled51444@gmail.com>
2024-05-27 21:18:44 +0300

    first commit

---
+++ day 00/homework/homework.py
@@ -0,0 +1,21 @@
+name = "data" 
+
+
+surname = "kirvalidze" 
+
+print(name) 
+#STR (STRING) TIPIS CVLADI
+age = 14 
+INT (INTEGER) MTELI RICXVI
+height =175.7 
+FLOOT TIPIS CVLADI ATWILADI
+#BOOLEAN (BOOL) TIPIS 
+knows_programming = True 
+is_ugly = False 
+print(name+" "+surname) 
+print(name + age) 
+print(tyoe(age))
+print(tyoe(name))
+print(tyoe(surname))
+print(tyoe(height))
+print(name + str(age))
---
+++ day 00/homework/homework1.py
@@ -0,0 +1,51 @@
+from turtle import *
+speed(10)
+
+#we want to paint a house
+
+#steo 1 draw a square
+color("dark red")
+width(7)
+forward(200)
+left(90)
+
+
+forward(200)
+
+
+left(90)
+forward(200)
+
+left(90)
+forward(200)
+#end of square
+
+#drawing a door
+
+left(90)
+
+forward(80)
+color("dark blue")
+
+left(90)
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+#end of door
+
+penup()
+goto(200,200)
+
+pendown()
+color("dark green")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+exitonclick()
